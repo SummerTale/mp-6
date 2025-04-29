345d178a92d304c02821d7012c7ebb9aae56e1c7
Kavya Verma <kavyaverma5601@gmail.com>
2025-04-29 19:54:26 -0400

    vercel fix

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import '../../app/globals.css';
 
-export default async function CallbackPage({searchParams,}:{searchParams:{ code?: string};}){
+export default async function CallbackPage({searchParams,}:{searchParams:{ code?: string | string[]};}){
   const params = await searchParams;
   const code = params.code;
 
